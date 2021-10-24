b2196bd6ece59e761a1f4d645d86c6677c9e1040
Vieiraork <vieiraork@gmail.com>
2021-10-24 16:58:20 -0300

    add variables type file

---
+++ 1 - variables-type.jl
@@ -0,0 +1,21 @@
+#=
+variables type:
+- Julia version: 1.0
+- Author: Vieiraork
+- Date: 2021-10-24
+=#
+
+# String
+string = "Exemple of string"
+
+# Character
+char = 'C'
+
+# Boolean
+result = true
+
+# Int
+number = 10
+
+# Float
+other_number = 2.7
