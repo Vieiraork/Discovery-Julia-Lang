7dee8b8fbc2fab6478c4d23385db11bb2c421502
Vieiraork <vieiraork@gmail.com>
2021-10-24 17:02:48 -0300

    add complex typles

---
+++ 2 - complex-types.jl
@@ -0,0 +1,17 @@
+#=
+dict:
+- Julia version: 1.0
+- Author: Vieiraork
+- Date: 2021-10-24
+=#
+
+# Dictionaries
+dict = Dict("Key" => "Value")
+println(typeof(dict))
+println(dict)
+
+# Tuples
+tuple = ("immutable object", "certify if you need to create this")
+
+# Arrays
+array = ["Exemple of array", 27, true, 1.75, ["Another array"], Dict("And dict" => "too")]
